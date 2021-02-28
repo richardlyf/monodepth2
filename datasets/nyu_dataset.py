0264f11a55cf002743cbae504b1d87884bd5c701
Richard Lin <yifengl@stanford.edu>
2021-02-27 23:51:34 -0800

    Add NYU dataloader

---
+++ datasets/nyu_dataset.py
@@ -0,0 +1,58 @@
+# Copyright Niantic 2019. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the Monodepth2 licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import skimage.transform
+import numpy as np
+import PIL.Image as pil
+
+from kitti_utils import generate_depth_map
+from .mono_dataset import MonoDataset
+
+
+class NYUDataset(MonoDataset):
+    """Superclass for different types of KITTI dataset loaders
+    """
+    def __init__(self, *args, **kwargs):
+        super(NYUDataset, self).__init__(*args, **kwargs)
+
+        # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size    
+        self.K = np.array([[5.1885790117450188e+02, 0, 3.2558244941119034e+02, 0],
+                           [0, 5.1946961112127485e+02, 2.5373616633400465e+02, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]], dtype=np.float32)
+        self.K[0, :] /= self.width
+        self.K[1, :] /= self.height
+        self.archive = np.load(os.path.join(self.data_path, "nyu_archive.npy"), allow_pickle=True).item()
+
+    def check_depth(self):
+    """We don't include depth data, because we can't use the mat function to reproject depth to rgb
+    """
+        return False
+
+    def get_color(self, folder, frame_index, side, do_flip):
+        color = self.loader(self.get_image_path(folder, frame_index))
+
+        if do_flip:
+            color = color.transpose(pil.FLIP_LEFT_RIGHT)
+
+        return color
+
+class NYUDepthDataset(NYUDataset):
+    """KITTI dataset which uses the updated ground truth depth maps
+    """
+    def __init__(self, *args, **kwargs):
+        super(NYUDepthDataset, self).__init__(*args, **kwargs)
+
+    def get_image_path(self, folder, frame_index):
+        image_name = archive[folder][frame_index][1][:-3] + 'jpg'
+        image_path = os.path.join(
+            self.data_path,
+            folder,
+            image_name)
+        return image_path
